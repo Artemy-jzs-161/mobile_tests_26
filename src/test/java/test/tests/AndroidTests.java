33240ba50b40c4ef9dfc999c2de8286f42f83341
Artemy-jzs-161 <borovik.artemy@yandex.ru>
2024-06-12 23:17:18 +0700

    add AndroidTests.java param

--- src/test/java/test/tests/AndroidTests.java
+++ src/test/java/test/tests/AndroidTests.java
@@ -2,38 +2,27 @@ package test.tests;
 
 import io.appium.java_client.AppiumBy;
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
 import static com.codeborne.selenide.Selenide.*;
 import static io.qameta.allure.Allure.step;
 
 public class AndroidTests extends TestBase {
-
-    @Test
+    @ValueSource(strings = {
+            "Java", "Appium"
+    })
+    @ParameterizedTest(name = "Проверка наличия элементов в результатах поиска {0}")
     @Tag("android")
-    @DisplayName("Проверка наличия элементов в результатах поиска")
-    void successfulSearchAppiumTest() {
-        step("Ввести в строку поиска значение Appium", () -> {
+    void successfulSearchTest(String searchQuery) {
+        step("Ввести в строку поиска значение {0}", () -> {
             $(AppiumBy.accessibilityId("Search Wikipedia")).click();
-            $(AppiumBy.id("org.wikipedia.alpha:id/search_src_text")).sendKeys("Appium");
+            $(AppiumBy.id("org.wikipedia.alpha:id/search_src_text")).sendKeys(searchQuery);
         });
-
-        step("Проверить, что контент найден", () ->
-                $$(AppiumBy.id("org.wikipedia.alpha:id/page_list_item_container"))
-                        .shouldHave(sizeGreaterThan(0)));
-    }
-
-    @Test
-    @Tag("android")
-    @DisplayName("Проверка наличия элементов в результатах поиска")
-    void successfulSearchJavaTest() {
-        step("Ввести в строку поиска значение Java", () -> {
-            $(AppiumBy.accessibilityId("Search Wikipedia")).click();
-            $(AppiumBy.id("org.wikipedia.alpha:id/search_src_text")).sendKeys("Java");
-        });
-
         step("Проверить, что контент найден", () ->
                 $$(AppiumBy.id("org.wikipedia.alpha:id/page_list_item_container"))
                         .shouldHave(sizeGreaterThan(0)));
     }
 }
+
