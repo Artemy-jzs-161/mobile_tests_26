1bf29063119b1b97e8c3c3e3f9b1be78d4054846
Artemy-jzs-161 <borovik.artemy@yandex.ru>
2024-06-14 01:04:52 +0700

    add emulator

---
+++ src/test/java/test/config/EmulatorConfig.java
@@ -0,0 +1,30 @@
+package test.config;
+
+import org.aeonbits.owner.Config;
+
+@Config.Sources({"classpath:${env}.properties"})
+public interface EmulatorConfig  extends Config {
+    @Key("appiumServer")
+    String appiumServer();
+
+    @Key("deviceName")
+    String deviceName();
+
+    @Key("platformName")
+    String platformName();
+
+    @Key("platformVersion")
+    String platformVersion();
+
+    @Key("appURL")
+    String appURL();
+
+    @Key("appPath")
+    String appPath();
+
+    @Key("appPackage")
+    String appPackage();
+
+    @Key("appActivity")
+    String appActivity();
+}
--- src/test/java/test/tests/TestBase.java
+++ src/test/java/test/tests/TestBase.java
@@ -2,18 +2,32 @@ package test.tests;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
+
 import io.qameta.allure.selenide.AllureSelenide;
 
 import test.drivers.BrowserstackDriver;
+import test.drivers.EmulatorDriver;
 import test.helpers.Attach;
+
 import org.junit.jupiter.api.*;
 
 import static com.codeborne.selenide.Selenide.*;
 
 public class TestBase {
+    public static String deviceHost = System.getProperty("deviceHost", "browserstack");
+
     @BeforeAll
-    static void beforeAll() {
-        Configuration.browser = BrowserstackDriver.class.getName();
+    static void beforeAll() throws Exception {
+        switch (deviceHost) {
+            case "browserstack":
+                Configuration.browser = BrowserstackDriver.class.getName();
+                break;
+            case "emulation":
+                Configuration.browser = EmulatorDriver.class.getName();
+                break;
+            default:
+                throw new Exception("Unrecognised deviceHost");
+        }
         Configuration.browserSize = null;
     }
 
@@ -27,7 +41,11 @@ public class TestBase {
     void addAttachments() {
         String sessionId = sessionId().toString();
         Attach.pageSource();
+
+        if (deviceHost.equals("browserstack")) {
+            Attach.addVideo(sessionId);
+        }
+
         closeWebDriver();
-        Attach.addVideo(sessionId);
     }
 }
